a7ef648276d37a89e01642a5f92c82152692dad5
Januarius93 <mstyczen@gmail.com>
2019-04-01 01:37:13 +0200

    trying to fix firefox

--- src/test/java/webautomation/utils/browser/OperaBrowser.java
+++ src/test/java/webautomation/utils/browser/OperaBrowser.java
@@ -7,28 +7,29 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Optional;
 
 import java.io.File;
 
 public class OperaBrowser implements Browser {
+    // FIXME: 4/1/2019
+
     @Override
     public WebDriver getDriver() {
         OperaDriverManager.getInstance().setup();
-        System.setProperty("webdriver.opera.driver", "C:\\Users\\mstyczen\\Documents\\operadriver_win64\\operadriver.exe");
         DesiredCapabilities dc = DesiredCapabilities.opera();
-//        OperaOptions operaOptions = new OperaOptions();
-//        operaOptions.setBinary(new File("C:\\Users\\mstyczen\\AppData\\Local\\Programs\\Opera\\launcher.exe"));
-//        operaOptions.addArguments("start-maximized"); // open Browser in maximized mode
-//        operaOptions.addArguments("disable-infobars"); // disabling infobars
-//        operaOptions.addArguments("--disable-extensions"); // disabling extensions
-//        operaOptions.addArguments("--disable-gpu"); // applicable to windows os only
-//        operaOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-//        operaOptions.addArguments("--no-sandbox"); // Bypass OS security model
-//        operaOptions.addArguments("--headless"); // Bypass OS security model
-//
-//        dc.setCapability(ChromeOptions.CAPABILITY,operaOptions);
-//        dc.setBrowserName("opera");
-        dc.setCapability("opera.binary", "C:\\Users\\mstyczen\\AppData\\Local\\Programs\\Opera\\launcher.exe");
+        OperaOptions operaOptions = new OperaOptions();
+        operaOptions.addArguments("start-maximized"); // open Browser in maximized mode
+        operaOptions.addArguments("disable-infobars"); // disabling infobars
+        operaOptions.addArguments("--disable-extensions"); // disabling extensions
+        operaOptions.addArguments("--disable-gpu"); // applicable to windows os only
+        operaOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        operaOptions.addArguments("--no-sandbox"); // Bypass OS security model
+        operaOptions.addArguments("--headless"); // Bypass OS security model
+
+        dc.setCapability(ChromeOptions.CAPABILITY,operaOptions);
+        dc.setBrowserName("opera");
+
 
         return new OperaDriver(dc);
     }
